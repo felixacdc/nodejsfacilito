b82c94d843eca7f89d6deba714ce555733a3007b
felixacdc <pfmata01@gmail.com>
2016-09-14 01:06:51 -0600

    [add] 4.- Curso NodeJS - Encabezados y el objeto response :white_check_mark:

--- node_course/hola_html.js
+++ node_course/hola_html.js
@@ -6,8 +6,14 @@ var http = require("http"),
 http.createServer(function(req, res) {
 
 	fs.readFile("./index.html", function(err, html) {
-		res.write(html);
+		res.writeHead(200, {
+			"Content-Type": "application/json"
+		});
+		// JSON.stringify convierte un json a string
+		res.write(JSON.stringify({nombre: "Uriel", username: "uriel"}));
 		res.end();
 	}); // Forma asincrona
 	
 }).listen(8080);
+
+// curl -I localhost:8080: si lo escribimos en consola nos muestra el encabezado de la peticion
