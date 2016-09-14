2ee374504bb486351ed50cea817a371dd29f1ef4
felixacdc <pfmata01@gmail.com>
2016-09-13 23:29:24 -0600

    [modify] 3.- Curso NodeJS - Leer archivos y programación asíncrona :white_check_mark:

--- node_course/hola_html.js
+++ node_course/hola_html.js
@@ -1,9 +1,13 @@
 var http = require("http"),
 	fs = require("fs"); //modulo para optener el archivo
 
-var html = fs.readFileSync("./index.html");
+// var html = fs.readFileSync("./index.html"); // Forma sincrona
 
 http.createServer(function(req, res) {
-	res.write(html);
-	res.end();
+
+	fs.readFile("./index.html", function(err, html) {
+		res.write(html);
+		res.end();
+	}); // Forma asincrona
+	
 }).listen(8080);
