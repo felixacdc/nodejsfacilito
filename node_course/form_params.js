fd48f285c24aac8babb290a6e73f0a66da49f5c2
felixacdc <pfmata01@gmail.com>
2016-09-15 00:15:53 -0600

    [modify]  6.- Curso NodeJS - Leer parámetros de formulario :white_check_mark:

--- node_course/form_params.js
+++ node_course/form_params.js
@@ -8,23 +8,28 @@ http.createServer(function(req, res) {
 
 	fs.readFile("./index.html", function(err, html) {
 		var html_string = html.toString();
-		var arreglo_parametros = [];
+		var arreglo_parametros = [], parametros = {};
 
 		var variables = html_string.match(/[^\{\}]+(?=\})/g);
+		var nombre = "";
 
 		if ( req.url.indexOf("?") > 0 ) {
 			var url_data = req.url.split("?");
-			arreglo_parametros = url_data[1].split("&");
+			var arreglo_parametros = url_data[1].split("&");
 		}
 
 		for (var i = arreglo_parametros.length - 1; i >= 0; i--) {
 			var parametro = arreglo_parametros[i];
+
+			var param_data = parametro.split("=");
+
+			parametros[param_data[0]] = param_data[1];
 		}
 
 		for (var i = variables.length - 1; i >= 0; i--) {
-
-			var value = eval(variables[i]);
-			html_string = html_string.replace("{" + variables[i] + "}", value);
+			var variable = variables[i];
+			
+			html_string = html_string.replace("{" + variables[i] + "}", parametros[variable]);
 		}
 
 		res.writeHead(200, {
