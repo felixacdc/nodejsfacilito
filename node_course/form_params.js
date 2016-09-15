fe09a705c0a5ded44cb108d56a1a231e3327b485
felixacdc <pfmata01@gmail.com>
2016-09-15 10:37:41 -0600

    [add]  7.- Curso NodeJS - Exports (exercise) :white_check_mark:

--- node_course/form_params.js
+++ node_course/form_params.js
@@ -1,8 +1,10 @@
 var http = require("http"),
 	fs = require("fs"),
-	parser = require("./params_parser.js"); 
+	parser = require("./params_parser.js")
+	rendered = require("./render_view.js"); 
 
 var p = parser.parse;
+var ren = rendered.render;
 
 http.createServer(function(req, res) {
 
@@ -10,23 +12,14 @@ http.createServer(function(req, res) {
 	if ( req.url.indexOf("favicon.ico") > 0 ) { return; }
 
 	fs.readFile("./index.html", function(err, html) {
-		var html_string = html.toString();
 		
-		var variables = html_string.match(/[^\{\}]+(?=\})/g);
-
 		var parametros = p(req);
-
-		for (var i = variables.length - 1; i >= 0; i--) {
-			var variable = variables[i];
-			
-			html_string = html_string.replace("{" + variables[i] + "}", parametros[variable]);
-		}
 
 		res.writeHead(200, {
 			"Content-Type": "text/html"
 		});
 		
-		res.write(html_string);
+		res.write(ren(html, parametros));
 		res.end();
 	});
 	
