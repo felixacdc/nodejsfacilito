4dfa86c800bd535c9fabb7f1fd5fe134b62beaef
felixacdc <pfmata01@gmail.com>
2016-09-15 10:33:02 -0600

    [add]  7.- Curso NodeJS - Exports :white_check_mark:

--- node_course/form_params.js
+++ node_course/form_params.js
@@ -1,5 +1,8 @@
 var http = require("http"),
-	fs = require("fs"); 
+	fs = require("fs"),
+	parser = require("./params_parser.js"); 
+
+var p = parser.parse;
 
 http.createServer(function(req, res) {
 
@@ -8,23 +11,10 @@ http.createServer(function(req, res) {
 
 	fs.readFile("./index.html", function(err, html) {
 		var html_string = html.toString();
-		var arreglo_parametros = [], parametros = {};
-
+		
 		var variables = html_string.match(/[^\{\}]+(?=\})/g);
-		var nombre = "";
 
-		if ( req.url.indexOf("?") > 0 ) {
-			var url_data = req.url.split("?");
-			var arreglo_parametros = url_data[1].split("&");
-		}
-
-		for (var i = arreglo_parametros.length - 1; i >= 0; i--) {
-			var parametro = arreglo_parametros[i];
-
-			var param_data = parametro.split("=");
-
-			parametros[param_data[0]] = param_data[1];
-		}
+		var parametros = p(req);
 
 		for (var i = variables.length - 1; i >= 0; i--) {
 			var variable = variables[i];
