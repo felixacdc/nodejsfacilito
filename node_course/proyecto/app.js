e571483d2e7078f74c47517c2b78bce5beebb553
felixacdc <pfmata01@gmail.com>
2016-09-22 12:52:45 -0600

    [add] 17.- Curso NodeJS - Conexión a MongoDB :innocent:

--- node_course/proyecto/app.js
+++ node_course/proyecto/app.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var app = express();
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+	email: String,
+	password: String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
 
 app.use("/public", express.static("public"));
 app.use(bodyParser.json());
@@ -15,13 +28,19 @@ app.get("/", function (request, response) {
 });
 
 app.get("/login", function (request, response) {
-	response.render("login");
+	User.find(function(err, doc) {
+		console.log(doc);
+		response.render("login");
+	});
 });
 
 app.post("/users", function(request, response) {
-	console.log('Contraseña: ' + request.body.password);
-	console.log('Email: ' + request.body.email);
-	response.send("Recibimos tus datos");
+	var user = new User({email: request.body.email, password: request.body.password});
+
+	user.save(function() {
+		response.send("Guardamos tus datos");
+	});
+	
 });
 
 app.listen(8080);
