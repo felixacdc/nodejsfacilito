a4fa383a269efa9b70a649b6afd4908ec9d88e0a
felixacdc <pfmata01@gmail.com>
2016-09-22 11:18:31 -0600

    [add] 16.- Curso NodeJS - Leer parámetros :eyes:

--- node_course/proyecto/app.js
+++ node_course/proyecto/app.js
@@ -1,18 +1,27 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
+app.use("/public", express.static("public"));
+app.use(bodyParser.json());
+// extended define con que algoritmo va a hacer el parsin la libreria cuando es false no se puede parsear arreglos, objetos, etc.
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "jade");
 
-app.use("/estatico", express.static("public"));
-app.use(express.static("assets"));
-
-app.get("/", function (req, res) {
-	res.render("index");
+app.get("/", function (request, response) {
+	response.render("index");
 });
 
-app.get("/login", function (req, res) {
-	res.render("login");
+app.get("/login", function (request, response) {
+	response.render("login");
+});
+
+app.post("/users", function(request, response) {
+	console.log('Contraseña: ' + request.body.password);
+	console.log('Email: ' + request.body.email);
+	response.send("Recibimos tus datos");
 });
 
 app.listen(8080);
