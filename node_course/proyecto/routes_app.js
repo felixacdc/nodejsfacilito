a868285deab446c7f25c83708dfa70f2f6c70168
felixacdc <pfmata01@gmail.com>
2016-09-25 16:00:11 -0600

    [add] 29.- Curso NodeJS - Rutas para CRUD

--- node_course/proyecto/routes_app.js
+++ node_course/proyecto/routes_app.js
@@ -7,4 +7,32 @@ router.get("/", function (request, response) {
 	response.render("app/home");
 });
 
+/* REST */
+router.get("/imagenes/new", function(request, response) {
+	response.render('app/imagenes/new');
+});
+
+router.get("/imagenes/:id/edit", function(request, response) {
+
+});
+
+router.route("/imagenes/:id")
+	.get(function(request, response) {
+
+	})
+	.put(function(request, response) {
+
+	})
+	.delete(function(request, response) {
+
+	});
+
+router.route("/imagenes")
+	.get(function(request, response) {
+
+	})
+	.post(function(request, response) {
+
+	});
+
 module.exports = router;
