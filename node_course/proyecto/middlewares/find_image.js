fab008de667968adcc96aa64912aba4f550d8284
felixacdc <pfmata01@gmail.com>
2016-09-28 00:26:28 -0600

    [add] 38.- Curso NodeJS - Validar propietario de imagen :+1:

--- node_course/proyecto/middlewares/find_image.js
+++ node_course/proyecto/middlewares/find_image.js
@@ -1,10 +1,11 @@
 var Imagen = require("../models/imagenes");
+var owner_check = require("./image_permission");
 
 module.exports = function(request, response, next) {
 	Imagen.findById(request.params.id)
 	.populate("creator")
 	.exec(function(error, imagen) {
-		if ( imagen != null ) {
+		if ( imagen != null && owner_check(imagen, request, response) ) {
 			console.log("Encontre la imagen");
 			response.locals.imagen = imagen;
 			next();
